b1bd03776b67ff6d9db8585d993501f38bb646e4
johnriforgiate <johnriforgiate@ksu.edu>
2019-03-05 16:18:57 -0600

    Implemented fake functions assignment reccomends

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -88,7 +88,8 @@ start_process (void *file_name_)
 int
 process_wait (tid_t child_tid UNUSED) 
 {
-  return -1;
+  while(1);
+  //return -1;
 }
 
 /* Free the current process's resources. */
@@ -437,7 +438,7 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE;
+        *esp = PHYS_BASE - 12; // TODO: change to actually accept arguments
       else
         palloc_free_page (kpage);
     }
