b4789ce496b0fd14bca29e8fa8f9616830b743a2
johnriforgiate <johnriforgiate@ksu.edu>
2019-03-05 17:16:14 -0600

    Added memory access (kind of)

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,7 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -15,6 +16,16 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
+  verify_pointer(f->esp);
+  int syscallNumber = (int)f->esp;
+  // TODO: dereference 
   printf ("system call!\n");
   thread_exit ();
+}
+
+void
+verify_pointer(void *vaddr)
+{
+  if(NULL == vaddr||is_kernel_vaddr(vaddr))// TODO: Check if address is outside user defined memory
+    thread_exit ();
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -2,5 +2,5 @@
 #define USERPROG_SYSCALL_H
 
 void syscall_init (void);
-
+void verify_pointer(void *);
 #endif /* userprog/syscall.h */
